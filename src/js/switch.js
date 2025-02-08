2308994de7c5cc19a1214d0e3bef73da538e9f03
longspur <etourvol@gmail.com>
2025-02-08 13:37:34 -0500

    fixing default theme

--- src/js/switch.js
+++ src/js/switch.js
@@ -2,7 +2,7 @@ function changeStyle(style) {
     if (style == null)
         style = localStorage.getItem("style");
         if (style == null) 
-            style = "/css/style.css";
+            style = "/css/style";
     document.querySelector("link[type='text/css']").setAttribute("href", style + ".css");
     localStorage.setItem("style", style);
 }
